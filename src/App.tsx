20748091c20d6e3af17ca55ceb9adebbc21bf55b
clarencepenz <clarencedespenz@gmail.com>
2021-12-05 17:55:55 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,382 @@
+import * as React from "react";
+import styled from "styled-components";
+import Web3 from "web3";
+
+import Web3Modal from "web3modal";
+// @ts-ignore
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+import Button from "./components/Button";
+import Column from "./components/Column";
+import Wrapper from "./components/Wrapper";
+import Modal from "./components/Modal";
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+import ModalResult from "./components/ModalResult";
+import AccountAssets from "./components/AccountAssets";
+import ConnectButton from "./components/ConnectButton";
+
+import { apiGetAccountAssets } from "./helpers/api";
+import {
+  formatTestTransaction,
+  getChainData
+} from "./helpers/utilities";
+import { IAssetData } from "./helpers/types";
+import { fonts } from "./styles";
+import {
+  ETH_SEND_TRANSACTION
+} from "./constants";
+
+const SLayout = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  text-align: center;
+`;
+
+const SContent = styled(Wrapper)`
+  width: 100%;
+  height: 100%;
+  padding: 0 16px;
+`;
+
+const SContainer = styled.div`
+  height: 100%;
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  word-break: break-word;
+`;
+
+const SLanding = styled(Column)`
+  height: 600px;
+`;
+
+const SModalContainer = styled.div`
+  width: 100%;
+  position: relative;
+  word-wrap: break-word;
+`;
+
+const SModalTitle = styled.div`
+  margin: 1em 0;
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const SModalParagraph = styled.p`
+  margin-top: 30px;
+`;
+
+// @ts-ignore
+const SBalances = styled(SLanding)`
+  height: 100%;
+  & h3 {
+    padding-top: 30px;
+  }
+`;
+
+const STestButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const STestButton = styled(Button)`
+  border-radius: 8px;
+  font-size: ${fonts.size.medium};
+  height: 44px;
+  width: 100%;
+  max-width: 175px;
+  margin: 12px;
+`;
+
+interface IAppState {
+  fetching: boolean;
+  address: string;
+  web3: any;
+  provider: any;
+  connected: boolean;
+  chainId: number;
+  networkId: number;
+  assets: IAssetData[];
+  showModal: boolean;
+  pendingRequest: boolean;
+  result: any | null;
+}
+
+const INITIAL_STATE: IAppState = {
+  fetching: false,
+  address: "",
+  web3: null,
+  provider: null,
+  connected: false,
+  chainId: 1,
+  networkId: 1,
+  assets: [],
+  showModal: false,
+  pendingRequest: false,
+  result: null
+};
+
+function initWeb3(provider: any) {
+  const web3: any = new Web3(provider);
+
+  web3.eth.extend({
+    methods: [
+      {
+        name: "chainId",
+        call: "eth_chainId",
+        outputFormatter: web3.utils.hexToNumber
+      }
+    ]
+  });
+
+  return web3;
+}
+
+class App extends React.Component<any, any> {
+  // @ts-ignore
+  public web3Modal: Web3Modal;
+  public state: IAppState;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      ...INITIAL_STATE
+    };
+
+    this.web3Modal = new Web3Modal({
+      network: this.getNetwork(),
+      cacheProvider: true,
+      providerOptions: this.getProviderOptions()
+    });
+  }
+
+  public componentDidMount() {
+    if (this.web3Modal.cachedProvider) {
+      this.onConnect();
+    }
+  }
+
+  public onConnect = async () => {
+    const provider = await this.web3Modal.connect();
+
+    await this.subscribeProvider(provider);
+
+    const web3: any = initWeb3(provider);
+
+    const accounts = await web3.eth.getAccounts();
+
+    const address = accounts[0];
+
+    const networkId = await web3.eth.net.getId();
+
+    const chainId = await web3.eth.chainId();
+
+    await this.setState({
+      web3,
+      provider,
+      connected: true,
+      address,
+      chainId,
+      networkId
+    });
+    await this.getAccountAssets();
+  };
+
+  public subscribeProvider = async (provider: any) => {
+    if (!provider.on) {
+      return;
+    }
+    provider.on("close", () => this.resetApp());
+    provider.on("accountsChanged", async (accounts: string[]) => {
+      await this.setState({ address: accounts[0] });
+      await this.getAccountAssets();
+    });
+    provider.on("chainChanged", async (chainId: number) => {
+      const { web3 } = this.state;
+      const networkId = await web3.eth.net.getId();
+      await this.setState({ chainId, networkId });
+      await this.getAccountAssets();
+    });
+
+    provider.on("networkChanged", async (networkId: number) => {
+      const { web3 } = this.state;
+      const chainId = await web3.eth.chainId();
+      await this.setState({ chainId, networkId });
+      await this.getAccountAssets();
+    });
+  };
+
+  public getNetwork = () => getChainData(this.state.chainId).network;
+
+  public getProviderOptions = () => {
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          infuraId: process.env.REACT_APP_INFURAID
+        }
+      },
+    };
+    return providerOptions;
+  };
+
+  public getAccountAssets = async () => {
+    const { address, chainId } = this.state;
+    this.setState({ fetching: true });
+    try {
+      // get account balances
+      const assets = await apiGetAccountAssets(address, chainId);
+
+      await this.setState({ fetching: false, assets });
+    } catch (error) {
+      console.error(error); // tslint:disable-line
+      await this.setState({ fetching: false });
+    }
+  };
+
+  public toggleModal = () =>
+    this.setState({ showModal: !this.state.showModal });
+
+  public testSendTransaction = async () => {
+    const { web3, address, chainId } = this.state;
+
+    if (!web3) {
+      return;
+    }
+
+    const tx = await formatTestTransaction(address, chainId);
+
+    try {
+      // open modal
+      this.toggleModal();
+
+      // toggle pending request indicator
+      this.setState({ pendingRequest: true });
+
+      // @ts-ignore
+      function sendTransaction(_tx: any) {
+        return new Promise((resolve, reject) => {
+          web3.eth
+            .sendTransaction(_tx)
+            .once("transactionHash", (txHash: string) => resolve(txHash))
+            .catch((err: any) => reject(err));
+        });
+      }
+
+      // send transaction
+      const result = await sendTransaction(tx);
+
+      // format displayed result
+      const formattedResult = {
+        action: ETH_SEND_TRANSACTION,
+        txHash: result,
+        from: address,
+        to: address,
+        value: "0 ETH"
+      };
+
+      // display result
+      this.setState({
+        web3,
+        pendingRequest: false,
+        result: formattedResult || null
+      });
+    } catch (error) {
+      console.error(error); // tslint:disable-line
+      this.setState({ web3, pendingRequest: false, result: null });
+    }
+  };
+
+
+  public resetApp = async () => {
+    const { web3 } = this.state;
+    if (web3 && web3.currentProvider && web3.currentProvider.close) {
+      await web3.currentProvider.close();
+    }
+    await this.web3Modal.clearCachedProvider();
+    this.setState({ ...INITIAL_STATE });
+  };
+
+  public render = () => {
+    const {
+      assets,
+      address,
+      connected,
+      chainId,
+      fetching,
+      showModal,
+      pendingRequest,
+      result
+    } = this.state;
+    return (
+      <SLayout>
+        <Column maxWidth={1000} spanHeight>
+          <Header
+            connected={connected}
+            address={address}
+            chainId={chainId}
+            killSession={this.resetApp}
+          />
+          <SContent>
+            {fetching ? (
+              <Column center>
+                <SContainer>
+                  <Loader />
+                </SContainer>
+              </Column>
+            ) : !!assets && !!assets.length ? (
+              <SBalances>
+                <h3>Actions</h3>
+                <Column center>
+                  <STestButtonContainer>
+                    <STestButton left onClick={this.testSendTransaction}>
+                      {ETH_SEND_TRANSACTION && 'Send Transaction'}
+                    </STestButton>
+                  </STestButtonContainer>
+                </Column>
+                <h3>Balances</h3>
+                <AccountAssets chainId={chainId} assets={assets} />{" "}
+              </SBalances>
+            ) : (
+              <SLanding center>
+                <h3>{`Cipher Tests Web3Modal`}</h3>
+                <ConnectButton onClick={this.onConnect} />
+              </SLanding>
+            )}
+          </SContent>
+        </Column>
+        <Modal show={showModal} toggleModal={this.toggleModal}>
+          {pendingRequest ? (
+            <SModalContainer>
+              <SModalTitle>{"Pending Call Request"}</SModalTitle>
+              <SContainer>
+                <Loader />
+                <SModalParagraph>
+                  {"Approve or reject request using your wallet"}
+                </SModalParagraph>
+              </SContainer>
+            </SModalContainer>
+          ) : result ? (
+            <SModalContainer>
+              <SModalTitle>{"Call Request Approved"}</SModalTitle>
+              <ModalResult>{result}</ModalResult>
+            </SModalContainer>
+          ) : (
+            <SModalContainer>
+              <SModalTitle>{"Call Request Rejected"}</SModalTitle>
+            </SModalContainer>
+          )}
+        </Modal>
+      </SLayout>
+    );
+  };
+}
+
+export default App;
---
+++ src/constants/actions.ts
@@ -0,0 +1,1 @@
+export const ETH_SEND_TRANSACTION = 'eth.sendTransaction()'
